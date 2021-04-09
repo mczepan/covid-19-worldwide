72484e2075144f6d2ed1c68a0abc6dc49d6bbcdd
Maciej Czepan <maciej.czepan@gmail.com>
2021-04-09 17:28:12 +0200

    material ui installed & header implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { MenuItem, FormControl, Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState('worldwide');
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, [countries]);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setSelectedCountry(countryCode);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value={selectedCountry}
+            onChange={onCountryChange}
+          >
+            <MenuItem value={'worldwide'}>Worldwide</MenuItem>
+            {countries.map((country, index) => (
+              <MenuItem key={index} value={country.value}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {/* {Header} */}
+      {/* {Title + Select input dropdown field} */}
+
+      {/* {InfoBoxes} */}
+      {/* {InfoBoxes} */}
+      {/* {InfoBoxes} */}
+
+      {/* {Table} */}
+      {/* {Graph} */}
+
+      {/* {Map} */}
     </div>
   );
 }
